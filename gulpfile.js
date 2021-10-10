c96f5147d464012df10c94d218965dce971b0bd1
vladk7072 <vladk7072@gmail.com>
2021-10-10 15:12:48 +0300

    added section home

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
