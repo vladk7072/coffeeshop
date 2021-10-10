581ff83760b6159e4ef8ef04ea0844d3ff7dd270
vladk7072 <vladk7072@gmail.com>
2021-10-10 20:55:35 +0300

    added section machine and carte

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-  
+    $(".star").rateYo({
+      rating: 4,
+      starWidth: "17px",
+      ratedFill: "#C99E71",
+      spacing: "7px"
+    });
 });
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
